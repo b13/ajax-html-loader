006de38fd807052eb26631d84bb1a25db88a6ae5
Benjamin Gröner <bgroener@Benjis-MacBook.local>
2015-07-22 12:35:01 +0200

    [TASK]Created test for loading handlers

--- test/dist/js/test.js
+++ test/dist/js/test.js
@@ -22,6 +22,23 @@ require([
 			new AjaxLoader(this);
 		});
 
+		$('.mJS_ajaxLoaderWithHandlers').each(function(){
+			new AjaxLoader(this,{
+				beforeLoading: [
+					function(){alert("BEFORE LOADING");}
+				],
+				ajaxSource: function(){
+					var randomNumber = Math.floor(Math.random() * 10);
+					if(randomNumber % 2 == 0) return "/ajax-source.html";
+					return "/invalidPath.html";
+				},
+				onLoading: function(){alert("ON LOADING")},
+				onLoadingSuccess: function(){alert("ON LOADING SUCCESS")},
+				onLoadingError: function(){alert("ON LOADING ERROR")}
+
+			})
+		});
+
 	}
 
 	// document ready
